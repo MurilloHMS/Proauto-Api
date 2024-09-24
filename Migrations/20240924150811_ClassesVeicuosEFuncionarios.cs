f772564539986de9f1b163e36c677b90cbeec35d
MurilloHMS <Murillohenriquemelo@outlook.com>
2024-09-24 12:20:29 -0300

    Incluido migrations

---
+++ Migrations/20240924150811_ClassesVeicuosEFuncionarios.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Proauto_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ClassesVeicuosEFuncionarios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: false),
+                    Departamento = table.Column<string>(type: "text", nullable: true),
+                    Hash = table.Column<string>(type: "text", nullable: true),
+                    Gerente = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Brand = table.Column<string>(type: "text", nullable: false),
+                    Model = table.Column<string>(type: "text", nullable: false),
+                    Plate = table.Column<string>(type: "text", nullable: false),
+                    VehicleCategory = table.Column<string>(type: "text", nullable: false),
+                    UrbanAlcoholConsumption = table.Column<double>(type: "double precision", nullable: false),
+                    RoadAlcoholConsumption = table.Column<double>(type: "double precision", nullable: false),
+                    UrbanGasolineConsumption = table.Column<double>(type: "double precision", nullable: false),
+                    RoadGasolineConsumption = table.Column<double>(type: "double precision", nullable: false),
+                    Status = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+        }
+    }
+}
